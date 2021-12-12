7db786445c65e4369f87b108d6476ce046b7ea6a
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2021-12-11 22:25:45 -0500

    feat: clock counter styles
    Still in WIP to mock correctly one from figma, and still in wip for mobile version.
    
    ToDo: queries for mobile, fix styles.

--- src/Components/Component/Countdown.component.jsx
+++ src/Components/Component/Countdown.component.jsx
@@ -14,17 +14,62 @@ const CountdownComponent = () => {
 
   // const [hello, setHello] = useState('Hello World!');
 
-  const BuyNow = () => {
-    return <span>Hello</span>
-  };
+  const renderer = ({days,hours, minutes, seconds, completed}) => {
+    if (completed) {
+      // Render a completed state
+      return <BuyNow/>;
+    } else {
+      // Render a countdown
+      return (
 
-  return (
-    <Fragment>
-      <Countdown date={Date.now() + 4000}>
-        <BuyNow />
-      </Countdown>
-    </Fragment>
-  );
-};
+        // <span className={'clock'}>
+        // <h1>{hours}</h1> : <h2>{minutes}</h2>: <h3>{seconds}</h3>
+        // </span>
 
-export default CountdownComponent;
+        // <div className="clock-input">
+        //   <input type="date" name="time-to" className="time-to" id="time-to" value="" onChange="calcTime(this.value)">
+        // </div>
+      <div className="container clock-container">
+        <div className="clock-column">
+          <p className="clock-day clock-timer">{days}</p>
+          <p className="clock-label">Days</p>
+        </div>
+
+        <div className="clock-column">
+          <p className="clock-hours clock-timer">{hours}</p>
+          <p className="clock-label">Hours</p>
+        </div>
+
+        <div className="clock-column">
+          <p className="clock-minutes clock-timer">{minutes}</p>
+          <p className="clock-label">Minutes</p>
+        </div>
+
+        <div className="clock-column">
+          <p className="clock-seconds clock-timer">{seconds}</p>
+          <p className="clock-label">Seconds</p>
+        </div>
+      </div>
+
+    );
+    }
+    }
+
+    const BuyNow = () =>
+      {
+        return <span>Hello</span>
+      }
+    ;
+
+    return (
+      <Fragment>
+        <Countdown
+          date={Date.now() + 400000000}
+          renderer={renderer}
+        >
+        </Countdown>
+      </Fragment>
+    );
+    };
+
+    export default CountdownComponent;
