6c69f938c80a6cef7f3a847f12b8b30bafd44020
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2021-12-11 22:24:37 -0500

    feat: drop zone logo for navbar
    drop zone logo in pure css and some fixes for navbar.
    
    styles to fix in mobile version WIP

--- src/Components/Component/Navbar.component.jsx
+++ src/Components/Component/Navbar.component.jsx
@@ -4,19 +4,19 @@ const NavbarComponent = () => {
   return (
     <nav className="navbar">
       <div className="container">
-        <div className="navbar-menu">
-          <div className="navbar-end">
-            <a className="navbar-item">
-              <div className="columns">
-                <div className="column" id='test-column'>
+        <div className="navbar-end navbar-end-force">
+          {/*<div className="navbar-end">*/}
+            <a className="navbar-item navbar-item-drop">
+              <div className="columns drop-zone">
+                <div className="column drop-content" id='test-column'>
                   Drop
                 </div>
-                <div className="column" id='test-column'>
+                <div className="column drop-content drop-content-end" id='test-column'>
                   Zone
                 </div>
               </div>
             </a>
-          </div>
+          {/*</div>*/}
         </div>
       </div>
     </nav>
