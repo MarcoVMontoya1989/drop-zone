3878fc6fed2fee204faa9dfa7369b3324bb0a359
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2021-12-15 15:58:26 -0500

    clean code: cleaning code for better understanding and structure

--- src/Components/Utils/arrayPad.js
+++ src/Components/Utils/arrayPad.js
@@ -1,8 +1,8 @@
-export const pad = (n, width, z) => {
-  z = z || '0';
-  n = n + '';
+export const pad = (num, width, z) => {
+  let zeroCount = z || '0';
+  let numDigit = num + '';
 
-  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  return numDigit.length >= width ? numDigit : new Array(width - numDigit.length + 1).join(zeroCount) + numDigit;
 };
 
 export default pad;
